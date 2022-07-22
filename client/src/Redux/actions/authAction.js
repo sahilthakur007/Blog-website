98d9357a6fb32e58e29bed2705164e598920b641
sahil thakur <st7378539838@gmail.com>
2022-07-22 23:16:25 +0530

    third

---
+++ client/src/Redux/actions/authAction.js
@@ -0,0 +1,14 @@
+export const loginUser = () => async (dispatch) => {
+    // api call 
+    dispatch({
+        type:"LOGIN"
+        //payload :
+    })
+}
+export const logoutUser = () => async (dispatch) => {
+    // api call 
+    dispatch({
+        type: "LOGOUT"
+        //payload :
+    })
+}
